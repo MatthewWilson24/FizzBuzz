491352d1bbac43de133fdaaafc28e67e94a48aec
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-11-19 15:05:53 +0000

    FizzBuzz Part 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,24 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	    for (int i = 1; i <= 100; i++)
-	    {
-	        System.out.print(fizzbuzz (i));
+	    for (int i = 1; i <= 100; i++) {
+	        System.out.println(fizzbuzz (i));
 	    }
     }
-}
+
+    public static String fizzbuzz (int n) {
+        if (n % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+        return Integer.toString(n);
+    }
+};
 
 
-public static String fizzbuzz (int n) {
-    if (n % 15 == 0) {
-        return "FizzBuzz";
-    }
-    if (n % 3 == 0) {
-        return "Fizz";
-    }
-    if (n % 5 == 0) {
-        return "Buzz";
-    }
-    return Integer.toString(n);
-}
